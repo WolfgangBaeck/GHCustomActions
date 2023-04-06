861d61076847b3b01a92bb119329f4eff8b64a68
Wolfgang Baeck <wbaeck@earthlink.net>
2023-04-06 09:32:21 -0600

    added js action to deploy to s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,21 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+
+    // get input values
+    const bucket = core.getInput('bucket', { required: true});
+    const bucketRegion = core.getInput('bucket-region', { required: true});
+    const distFolder = core.getInput('dist-filder', { required: true});
+
+    // upload files
+    const s3Uri = `s3://${bucket}`;
+    // provide AWS environment variables for security via the env variable on the step
+
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+
     core.notice('Hello from my custom javascipt action!');
 }
 
